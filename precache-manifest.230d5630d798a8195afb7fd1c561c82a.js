c3b6105690d3d8415a7f3e49dd21e43767998466
Vaibhav <vaibhavupadhyay5>
2020-02-09 15:04:56 +0530

    Updates

---
+++ precache-manifest.230d5630d798a8195afb7fd1c561c82a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9059c05576a154871a7234ccd4e81cd2",
+    "url": "/blahblah/index.html"
+  },
+  {
+    "revision": "81ec363ba815a501abbb",
+    "url": "/blahblah/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "43258051ec303a9d9fa5",
+    "url": "/blahblah/static/js/2.6f25374b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/blahblah/static/js/2.6f25374b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ec363ba815a501abbb",
+    "url": "/blahblah/static/js/main.1a81bf2d.chunk.js"
+  },
+  {
+    "revision": "ad0501e1ff4776d7896a",
+    "url": "/blahblah/static/js/runtime-main.453dc7b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blahblah/static/media/logo.5d5d9eef.svg"
+  }
+]);
